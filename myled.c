9b09ed3c838d800af42f48944f37930ce90767d4
yuyakobayashi7 <54259047+yuyakobayashi7@users.noreply.github.com>
2020-12-05 14:08:02 +0900

    インデントの修正

--- myled.c
+++ myled.c
@@ -73,11 +73,11 @@ static int __init init_mod(void)
 
 	for (i = 0; i < 3; i++){
 
-	const u32 led = led_number[i];
-	const u32 index = led/10;
-	const u32 shift = (led%10)*3;
-	const u32 mask = ~(0x7 << shift);
-	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+		const u32 led = led_number[i];
+		const u32 index = led/10;
+		const u32 shift = (led%10)*3;
+		const u32 mask = ~(0x7 << shift);
+		gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
 	}
 	
 	retval =  alloc_chrdev_region(&dev, 0, 1, "myled");
